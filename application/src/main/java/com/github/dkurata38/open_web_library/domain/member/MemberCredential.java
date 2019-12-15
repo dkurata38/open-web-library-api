17808d9a97f12849a3a09c14d8de4333d6bea03b
dkurata38 <d.kurata.38@gmail.com>
2019-12-15 14:19:54 +0900

    ID用の基底インターフェースを作成

--- application/src/main/java/com/github/dkurata38/open_web_library/domain/member/MemberCredential.java
+++ application/src/main/java/com/github/dkurata38/open_web_library/domain/member/MemberCredential.java
@@ -4,6 +4,9 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Embedded;
+import org.springframework.data.relational.core.mapping.Table;
 
 import static java.util.stream.Collectors.*;
 
@@ -12,7 +15,9 @@ import static java.util.stream.Collectors.*;
 @EqualsAndHashCode(of = {"memberCredentialId"})
 public class MemberCredential {
 	@Id
+	@Embedded.Empty
 	private final MemberCredentialId memberCredentialId;
+	@Embedded.Empty
 	private final MemberId memberId;
 	private final String loginId;
 	private final String password;
