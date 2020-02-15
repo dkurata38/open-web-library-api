3da56f67ed6ad729065684b1752fd7aa63efdc23
dkurata38 <d.kruata.38@gmail.com>
2020-02-16 01:40:49 +0900

    不要な依存性を削除

--- repository/build.gradle.kts
+++ repository/build.gradle.kts
@@ -23,7 +23,6 @@ configurations {
 
 dependencies {
     implementation(project(":domain"))
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:2.1.1")
     implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.1.4")
     implementation("com.github.dkurata38:domain-lib:1.0.0")
