2b3de325984dc2524714ffc1358f649db73fbd02
dkurata38 <d.kruata.38@gmail.com>
2020-02-16 21:28:22 +0900

    不要な記述を削除

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -40,14 +40,3 @@ tasks.withType<KotlinCompile> {
         jvmTarget = "11"
     }
 }
-repositories {
-    mavenCentral()
-}
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
