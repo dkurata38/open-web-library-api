bb7598adcc67ac910c7b922a762f3eaaeba9a644
dkurata38 <d.kruata.38@gmail.com>
2020-02-16 18:07:09 +0900

    リネーム

--- client/src/main/kotlin/com/github/dkurata38/open_web_library/client/ClientModule.kt
+++ client/src/main/kotlin/com/github/dkurata38/open_web_library/client/ClientModule.kt
@@ -1,4 +1,4 @@
-package com.github.dkurata38.open_web_library.client;
+package com.github.dkurata38.open_web_library.client
 
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate
 
 @ComponentScan(basePackages = ["com.github.dkurata38.open_web_library.client.*"])
 @Configuration
-class ClientConfig {
+class ClientModule {
 	@Bean
 	fun restTemplate(): RestTemplate {
 		return RestTemplateBuilder().build()
