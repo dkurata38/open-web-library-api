98204e8215dc095dca6f3b30b4c70e62c521e9ad
dkurata38 <d.kruata.38@gmail.com>
2020-02-24 23:54:41 +0900

    認証をインメモリで仮実装

--- web/src/main/kotlin/com/github/dkurata38/open_web_library/web/security/SecurityConfig.kt
+++ web/src/main/kotlin/com/github/dkurata38/open_web_library/web/security/SecurityConfig.kt
@@ -4,14 +4,18 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.authority.AuthorityUtils
 import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig : WebSecurityConfigurerAdapter() {
 	@Bean
 	fun passwordEncoder(): PasswordEncoder {
@@ -22,10 +26,17 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
 		http
 				.formLogin()
 				.defaultSuccessUrl("/book_collections", true)
+
 	}
 
 	override fun configure(auth: AuthenticationManagerBuilder) {
 		auth.inMemoryAuthentication()
-				.withUser(User("kurata", passwordEncoder().encode("0141suk3"), AuthorityUtils.createAuthorityList("USER")))
+				.withUser(User("kurata", passwordEncoder().encode("password"), AuthorityUtils.createAuthorityList("USER")))
+	}
+
+	@Bean
+	override fun userDetailsService(): UserDetailsService {
+		val user = User("kurata", passwordEncoder().encode("password"), AuthorityUtils.createAuthorityList("USER"))
+		return InMemoryUserDetailsManager(user)
 	}
 }
